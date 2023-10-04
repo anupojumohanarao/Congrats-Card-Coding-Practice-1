48a5146e99bbab89be77073178b1ad781a95c2fe
Anupoju Mohana Rao <mohananupoju14@gmail.com>
2023-10-04 07:52:46 +0000

    Congrats-Card-Solution

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div class="bg-container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="main-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="person-image"
+      />
+      <h1 class="person-name">Kiran V</h1>
+      <p class="topic-details">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="person-name pb-5"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
